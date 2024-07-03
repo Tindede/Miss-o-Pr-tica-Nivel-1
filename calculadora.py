2c8ef268a59bf6775c950e223884d996de918ac8
Tindede <163802399+Tindede@users.noreply.github.com>
2024-07-03 10:18:02 -0300

    Add files via upload
    Aqui estão todas as Microatividades e também a Missão Pratica do Nível 1.

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+a = int(input('Digite um número:'))
+
+b = int(input('Digite outro número:'))
+
+adiçao = a + b
+subtraçao = a - b
+multiplicaçao = a * b
+divisao = a / b
+
+print('O resultado da Adição é:' + str(adiçao))
+print('O resultado da Subtração é:' + str(subtraçao))
+print('O resultado da Multiplicação é:' + str(multiplicaçao))
+print('O resultado da Divisão é:' + str(divisao))
+
+
+
+
+
+
+
+
---
+++ entrada.py
@@ -0,0 +1,9 @@
+name = input('Digite seu nome:')
+
+idade = input('Digite sua idade:')
+
+print('Olá, ' + name + '. Você tem ' + idade + ' anos.')
+
+
+
+
